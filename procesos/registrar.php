5b281300ae31038d64d460a7556830410741b4b5
devos10 <osbaldo081004@gmail.com>
2025-11-12 21:48:07 -0600

    validando que los datos del formulario se envien por post y que ademas se regrese a la vista en caso de querer ingresar a una carpeta no valida para el usuario

--- procesos/registrar.php
+++ procesos/registrar.php
@@ -1,7 +1,15 @@
 <?php
 //verificar si se ha enviado el formulario
+// Evita errores si se entra por GET o sin datos
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 $usuario=$_POST['usuario'];
 $contraseña=$_POST['contraseña'];
 $contraseña_confirmar=$_POST['contraseña_confirmar'];
  print_r("Usuario: " . $usuario . " Contraseña: " . $contraseña. " Confirmar Contraseña: " . $contraseña_confirmar);
+
+} else {
+    // Si alguien entra directo a registrar.php,regresa al formulario
+    header('Location: ../registro.php'); 
+    exit;
+}
 ?>
