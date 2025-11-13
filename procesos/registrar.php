08ed93d77ef6ded51594fcc4ad03c2a3f85da4f4
devos10 <osbaldo081004@gmail.com>
2025-11-12 23:04:18 -0600

    agregando logica para verificacion de contraseñas iguales y mandando mensajes de creacion

--- procesos/registrar.php
+++ procesos/registrar.php
@@ -1,11 +1,24 @@
 <?php
+require_once __DIR__ . '/../db/UsuarioBD.php';
 //verificar si se ha enviado el formulario
 // Evita errores si se entra por GET o sin datos
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 $usuario=$_POST['usuario'];
-$contraseña=$_POST['contraseña'];
-$contraseña_confirmar=$_POST['contraseña_confirmar'];
- print_r("Usuario: " . $usuario . " Contraseña: " . $contraseña. " Confirmar Contraseña: " . $contraseña_confirmar);
+$contrasena=$_POST['contraseña'];
+$contrasena_confirmar=$_POST['contraseña_confirmar'];
+//validar que las contraseñas coincidan 
+    if($contrasena !== $contrasena_confirmar){
+        echo "Las contraseñas no coinciden.";
+        exit;
+    }else{
+        //crear el usuario
+        $resultado=crearUsuario($usuario,$contrasena);
+        if($resultado){
+            echo "Usuario creado exitosamente.";
+        }else{
+            echo "Error al crear el usuario.";
+        }
+    }
 
 } else {
     // Si alguien entra directo a registrar.php,regresa al formulario
